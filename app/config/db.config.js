50a77da6fdce6a61b43b463e664e7ea9c13b88cd
Manmohan506 <manmohan@zelarsoft.com>
2021-09-22 11:00:31 +0530

    ss

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,5 +1,4 @@
 const env = require('./env.js');
-require('dotenv').config();
  
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(env.database, env.username, env.password, {
@@ -8,10 +7,10 @@ const sequelize = new Sequelize(env.database, env.username, env.password, {
   operatorsAliases: false,
  
   pool: {
-    max: env.max,
-    min: env.min,
-    acquire: env.acquire,
-    idle: env.idle
+    max: 5 
+    min: 0
+    acquire: 30000
+    idle: 10000
   }
 });
 const db = {};
